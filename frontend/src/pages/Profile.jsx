37fdffd96984d43e3fd299013cda4145bd6d684f
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-25 22:21:57 +0500

    Add frontend/src/pages/Profile.jsx

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileInfo from '../components/ProfileInfo';
+
+function Profile() {
+  return (
+    <div className="container mx-auto p-4">
+      <ProfileInfo />
+    </div>
+  );
+}
+
+export default Profile;
