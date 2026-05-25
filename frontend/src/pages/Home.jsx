569ecd849a960b66f4155c18895747cc30771312
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-25 22:21:51 +0500

    Add frontend/src/pages/Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContent from '../components/HomeContent';
+
+function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <HomeContent />
+    </div>
+  );
+}
+
+export default Home;
