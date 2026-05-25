8cf4e977898dfccfd21145cfc38899a24be365fb
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-25 22:21:53 +0500

    Add frontend/src/pages/Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+function Login() {
+  return (
+    <div className="container mx-auto p-4">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
