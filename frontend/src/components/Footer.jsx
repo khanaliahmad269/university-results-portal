934084c4b3ac800b8bdb6c42c7f81f25b20491cc
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-25 22:21:49 +0500

    Add frontend/src/components/Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+      <div className="container mx-auto text-center">
+        &copy; 2023 University Results Portal. All rights reserved.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
