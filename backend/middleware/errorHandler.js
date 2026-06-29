1ad7083515f80f6755bf36b4697e7cda24c73c40
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:27 +0500

    Update backend/middleware/errorHandler.js via Code Vibe

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 function errorHandler(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+  res.status(500).send('Something broke!');
 }
 
 module.exports = errorHandler;
