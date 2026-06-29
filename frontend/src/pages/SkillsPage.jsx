3228837c3a5ce9b5e1c5d328b3640314a8076f59
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:08 +0500

    Add frontend/src/pages/SkillsPage.jsx via Code Vibe

---
+++ frontend/src/pages/SkillsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SkillsSection from '../components/SkillsSection';
+
+function SkillsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <SkillsSection />
+    </div>
+  );
+}
+
+export default SkillsPage;
