f647e79c183ae48785ba84c5481ca2e74f6d7545
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:04 +0500

    Add frontend/src/pages/AboutPage.jsx via Code Vibe

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+
+function AboutPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <AboutSection />
+    </div>
+  );
+}
+
+export default AboutPage;
