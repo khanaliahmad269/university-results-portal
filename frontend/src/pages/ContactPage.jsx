fee39ab9f3c6e8e7e74cb158679bedc437999ff5
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:10 +0500

    Add frontend/src/pages/ContactPage.jsx via Code Vibe

---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+
+function ContactPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default ContactPage;
