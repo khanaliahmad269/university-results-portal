06000b0e739a70eaf55d0fd09c8efbf7d2b86a6a
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:06 +0500

    Add frontend/src/pages/ProjectsPage.jsx via Code Vibe

---
+++ frontend/src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectsList from '../components/ProjectsList';
+
+function ProjectsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <ProjectsList />
+    </div>
+  );
+}
+
+export default ProjectsPage;
