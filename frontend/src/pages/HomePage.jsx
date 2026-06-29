49917e7ba2aa4cb9b79728aedd7eed2a32441163
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 06:29:02 +0500

    Add frontend/src/pages/HomePage.jsx via Code Vibe

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Introduction from '../components/Introduction';
+
+function HomePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <Introduction />
+    </div>
+  );
+}
+
+export default HomePage;
